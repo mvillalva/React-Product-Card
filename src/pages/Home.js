d4b792bdf5f33b1a50f8d4642205240698c88e78
mvillalva <maxi.villalva@gmail.com>
2023-02-10 08:44:25 -0300

    Mejora visual

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,8 +16,8 @@ const Home = () => {
   return (
     <>
       <div className='d-flex justify-content-center align-items-center flex-column'>
-        <h1>home</h1>
-        <div className='d-flex justify-content-center gap-5 flex-row'>
+        <h1 className='text-light'>Lista de Productos</h1>
+        <div className='d-flex justify-content-center gap-5 flex-row mt-5'>
           {/* Se pasa en la props 'prods' los datos del useState para poder manejarlo desde cada tarjeta de producto y se refleje el cambio a nivel global */}
           <ProductCard prods={{products, setProducts}} id={1} name="Máscara" desc="Producto 1. Acá iría una breve descripción del producto"/>
           <ProductCard prods={{products, setProducts}} id={2} name="Otra Máscara" desc="Producto 2. Acá iría una breve descripción del producto"/>
